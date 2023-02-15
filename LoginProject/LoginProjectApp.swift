b5593fdd63ec374858a052cab5d5d5556c022836
adiamoyal <adi.amoyal02@gmail.com>
2023-02-15 20:01:26 +0200

    Initial Commit

---
+++ LoginProject/LoginProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginProjectApp.swift
+//  LoginProject
+//
+//  Created by Adi Amoyal on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
